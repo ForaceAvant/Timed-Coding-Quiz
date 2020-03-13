0df846277e5f6feb95a19018530c235390a6d218
ForaceGump <themattavant@gmail.com>
2020-03-13 00:54:05 -0700

    Created questionObject{} to store all questions and answers for the quiz.

--- script.js
+++ script.js
@@ -7,6 +7,41 @@ var answerButton2 = document.querySelector("#answer-2");
 var answerButton3 = document.querySelector("#answer-3");
 var answerButton4 = document.querySelector("#answer-4");
 var beginButton = document.querySelector("#begin-button");
+//Object containing all the questions and answers
+var questionObject = {
+    question1 : {
+        question: "To declare a variable in Javascript you must use ____.",
+        answers: ["create","var","int/string/array/double","dec"]
+    },
+    question2 : {
+        question: "HTML stands for ____",
+        answers: ["Hyperbolic Theme Machine Language", "Hanging Time Marking Leverage", "Hyper Text Markup Language"]
+    },
+    question3 : {
+        question: "What is the Git command to see all files in the current directory?",
+        answers: ["ls","see file -a","cd","show directory"]
+    },
+    question4 : {
+        question: "What language is used to make the skin of all webpages?",
+        answers: ["HTML", "CSS", "Javascript","C++"]
+    },
+    question5 : {
+        question: "What language is used to make the muscle of all webpages?",
+        answers: ["HTML", "CSS", "Javascript","C++"]
+    },
+    question6 : {
+        question: "What language is used to make the bones of all webpages?",
+        answers: ["HTML", "CSS", "Javascript","C++"]
+    },
+    question7 : {
+        question: "What can a variable be described as?",
+        answers: ["A hammer", "A waterfall", "A wedge", "A container"]
+    },
+    question8 : {
+        question: "What does CSS stand for?",
+        answers: ["Careful Step by Step", "Create Super Scripts", "Cascading Style Sheets", "Combinatory Style Stanzas"]
+    }
+}
 
 //Creates <div> elements to help format button layout
 function addButtonDivs(button){
@@ -35,4 +70,5 @@ beginButton.addEventListener("click", function(){
     addButtonDivs(answerButton4);
 
     beginButton.style.display = "none";
-})
+});
+
