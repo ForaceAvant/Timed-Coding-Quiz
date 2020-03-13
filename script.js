7808aaa739cb2273088190d18368d51b1a1e6db8
ForaceGump <themattavant@gmail.com>
2020-03-13 00:27:17 -0700

    Created the function addButtonDivs() to help create new div elements for formatting the buttons

--- script.js
+++ script.js
@@ -8,12 +8,31 @@ var answerButton3 = document.querySelector("#answer-3");
 var answerButton4 = document.querySelector("#answer-4");
 var beginButton = document.querySelector("#begin-button");
 
+//Creates <div> elements to help format button layout
+function addButtonDivs(button){
+    var newDiv = document.createElement("div");
+    newDiv.append(button);
+    jumbotronElement.append(newDiv);
+}
 
-
+//Beginning Event Listener that starts on the click of Begin Button
 beginButton.addEventListener("click", function(){
     answerButton1.style.display = "inline-block";
+    answerButton1.textContent = "Filler Answer";
+    addButtonDivs(answerButton1);
+
+    
     answerButton2.style.display = "inline-block";
+    answerButton2.textContent = "Filler Answer";
+    addButtonDivs(answerButton2);
+
     answerButton3.style.display = "inline-block";
+    answerButton3.textContent = "Filler Answer";
+    addButtonDivs(answerButton3);
+
     answerButton4.style.display = "inline-block";
+    answerButton4.textContent = "Filler Answer";
+    addButtonDivs(answerButton4);
+
     beginButton.style.display = "none";
 })
